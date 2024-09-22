344d1afa20e21ddfb6199369d496f0f4858e416b
ilyesOussama <51187750+ilyesOussama@users.noreply.github.com>
2024-09-22 01:56:51 +0100

    env

---
+++ src/env/server.ts
@@ -0,0 +1,39 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { ZodError, z } from "zod";
+
+expand(config());
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+    AUTH_URL: z.string().url(),
+    /* VERCEL_URL: z.string().url(),
+    DOMAIN_URL: z.string().url(),
+     */ AUTH_SECRET: z.string(),
+    AUTH_GOOGLE_ID: z.string(),
+    AUTH_GOOGLE_SECRET: z.string(),
+    DB_HOST: z.string(),
+    DB_USER: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_NAME: z.string(),
+    DB_PORT: z.coerce.number(),
+    DATABASE_URL: z.string().url(),
+    DB_MIGRATING: z
+      .string()
+      .refine((s) => s === "true" || s === "false")
+      .transform((s) => s === "true")
+      .optional(),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors,
+    );
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+});
